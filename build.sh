805cf77bfcf61249db6c851223f7cfaf01f9283b
ibuki1805 <ee173131@meiji.ac.jp>
2022-06-15 14:30:09 +0900

    latest

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ eval `ssh-agent`
 ssh-add ~/.ssh/id_rsa
 echo `ssh-add -l`
 # docker build --no-cache -t brushee-camera-docker --ssh default .
-docker build  -t brushee-camera-docker --ssh default --progress=plain .
+docker build  -t ibukinishimura/brushee-camera-docker --ssh default --progress=plain .
 # docker buildx build  -t brushee-camera-docker --ssh default --platform x86_64 .
